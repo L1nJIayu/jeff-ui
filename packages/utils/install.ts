4e2d54d3e243e21a8c3c0a9219718f1de36682e0
L1nJIayu <562316920@qq.com>
2025-03-05 18:30:52 +0800

    create types pkg, create theme pkg

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,7 +1,19 @@
 
-export const componentInstall = (components) => {
-  components.install = (app) => {
-    app.component(app.name, components)
-  }
-  return components
-}
+// export const componentInstall = (components) => {
+//   components.install = (app) => {
+//     app.component(app.name, components)
+//   }
+//   return components
+// }
+import { App, Plugin } from "vue";
+
+export const componentInstall = <T extends Record<string, any>>(components: T & Plugin) => {
+  components.install = (app: App) => {
+    for (const key in components) {
+      if (components[key] && components[key].name) {
+        app.component(components[key].name, components[key]);
+      }
+    }
+  };
+  return components;
+};
---
+++ typings/env.d.ts
@@ -0,0 +1,14 @@
+
+declare module '@vue/runtime-core' {
+  export interface App {
+    [INSTALLED_KEY]?: boolean
+  }
+
+  export interface GlobalComponents {
+    Component: (props: { is: Component | string }) => void
+  }
+
+  export interface ComponentCustomProperties {
+    vShow: typeof vShow
+  }
+}
