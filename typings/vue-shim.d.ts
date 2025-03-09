57b3d53da3398ddecdc37fd083ac90b61a255510
L1nJIayu <562316920@qq.com>
2025-03-09 22:21:11 +0800

    update d.ts file

--- typings/vue-shim.d.ts
+++ typings/vue-shim.d.ts
@@ -1,21 +1,7 @@
-/// <reference types="vite/client" />
+// / <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { defineComponent, DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  return component
-}
-
-// declare module '@vue/runtime-core' {
-//   export interface App {
-//     [INSTALLED_KEY]?: boolean
-//   }
-
-//   export interface GlobalComponents {
-//     Component: (props: { is: Component | string }) => void
-//   }
-
-//   export interface ComponentCustomProperties {
-//     vShow: typeof vShow
-//   }
+// declare module '*.vue' {
+//   import type { defineComponent, DefineComponent } from 'vue'
+//   const component: DefineComponent<{}, {}, any>
+//   return component
 // }
