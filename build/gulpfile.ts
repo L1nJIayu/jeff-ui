d503a482fa07bcad482ae2303f66c535dfdce585
L1nJIayu <562316920@qq.com>
2025-03-21 18:25:20 +0800

    update:build gulpfile

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -5,16 +5,3 @@ export default series(
   withTaskName('remove dist', async () => run('rm -rf ./dist')),
   withTaskName('build packages', async () => run('pnpm run --filter ./packages --parallel build'))
 )
-/*
-
-  pnpm add -D
-  @types/gulp-autoprefixer
-  @types/gulp-clean-css
-  @types/gulp-sass
-  @types/sass 
-  gulp-autoprefixer
-  gulp-clean-css
-  gulp-sass
-  -w
-   
-*/
